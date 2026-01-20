210132d76afe7b4fda7b3ee8993b532d7d8a8f00
StevenKole <stevenkolenko@gmail.com>
2026-01-20 12:29:20 -0500

    Lab 01

---
+++ Week01/lab01.py
@@ -0,0 +1,19 @@
+# Sample Coding Questions 01 Week 01
+# Steven Kolenko
+
+vars = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = ((a - ((b ** c) // d)) + (a % c))
+
+temperature = 32.6
+output = f"The temperature today is: {temperature:.3f} degrees Celsius"
+print(output)
+
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print(f"Now showing thee shop items filtered by age: {userAge} ")
